f4f3295adcbb094a3ffd9ed4bb2b48b24d8041e6
unknown <blessarnaud@gmail.com>
2023-02-16 15:30:25 +0100

    Modal signupform / theme / router

---
+++ src/components/auth/SignupForm.js
@@ -0,0 +1,80 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Style from "./auth.module.css";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from '@mui/material/Link';
+
+export default function FormPropsTextFields() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function signup() {
+
+    const credentials = {email, password};
+
+    fetch('http://localhost:3001/api/auth/signup/', {
+      method: 'POST',
+      body: JSON.stringify(credentials),
+    
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    
+    })
+  }
+
+  return (
+    <>
+      <Box className={Style.title}>
+        <Typography variant="h2" gutterBottom>
+          S'inscrire
+        </Typography>
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "50%" },
+        }}
+        noValidate
+        autoComplete="off"
+        className={Style.formContainer}
+      >
+        <div className={Style.inputForm}>
+          <TextField
+            fullWidth
+            required
+            id="outlined-required"
+            label="Email"
+            type="email"
+            className={Style.fieldForm}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={Style.inputForm}>
+          <TextField
+            fullWidth
+            id="outlined-password-input"
+            label="Mot de passe"
+            type="password"
+            autoComplete="current-password"
+            className={Style.fieldForm}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Link href="/connexion" color="inherit" sx={{mb:3}}>Déjà un compte ? Se connecter</Link>
+        <Button
+          onClick={() => signup()}
+          variant="contained"
+          color="success"
+        >
+          Valider
+        </Button>
+      </Box>
+    </>
+  );
+}
