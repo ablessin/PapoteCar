79304a988cdcb06d68702acab2dd788344d779b2
unknown <blessarnaud@gmail.com>
2023-02-17 13:55:35 +0100

    fixing mege

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inscription from "../pages/Inscription";
+import Connexion from "../pages/Login"
 
 const Router = () => {
   return (
@@ -7,6 +8,7 @@ const Router = () => {
       <Routes>
         <Route>
           <Route exact path={"/inscription"} element={<Inscription />} />
+          <Route exact path={"/connexion"} element={<Connexion />} />
         </Route>
       </Routes>
     </BrowserRouter>
