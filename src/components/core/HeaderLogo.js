5a894b093fae201be8295a662f99d95fa11b56e6
unknown <blessarnaud@gmail.com>
2023-03-09 15:49:11 +0100

    update theme / home

--- src/components/core/HeaderLogo.js
+++ src/components/core/HeaderLogo.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Box, Container, Grid, Typography } from '@mui/material';
-import '../../assets/css/views.css';
+import React from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import "../../assets/css/views.css";
 import CardTrip from "../CardTrip";
 
 // COMPONENT HEADER DU SITE
 function HeaderLogo() {
-
-    return (
-        <Box className="header">
-            <Container>
-                <Grid container justifyContent="center">
-                    <Grid item xs={12} md={10}>
-                        <Typography variant="h1">
-                            <CardTrip ></CardTrip>
-                        </Typography>
-                        <Typography sx={{ color: "white"}} variant="h2">Bienvenue sur GreenGo</Typography>
-                        <Typography sx={{ color: "white"}} variant="h5">Le moyen le plus économique de faire du co-voiturage</Typography>
-                    </Grid>
-                </Grid>
-            </Container>
-        </Box>
-    );
+  return (
+    <Box className="header">
+      <Container>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={10}>
+            <Typography variant="h1">
+              <CardTrip></CardTrip>
+            </Typography>
+            <Typography sx={{ color: "white" }} variant="h1">
+              Bienvenue sur GreenGo
+            </Typography>
+            <Typography sx={{ color: "white" }} variant="h5">
+              Le moyen le plus économique de faire du co-voiturage
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
 
 export default HeaderLogo;
