4e58e136d11c9a2d3a4f88fbbcfdcb88cb8ca2ef
unknown <blessarnaud@gmail.com>
2023-04-11 11:50:06 +0200

    resolve footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./App.css";
-import {ThemeProvider} from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "./configuration/themes/theme";
 import Router from "./router";
 import ButtonAppBar from "./components/core/Navbar";
 import Footer from "./components/core/Footer";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <ButtonAppBar/>
-            <Router/>
-            <Box mt={5} mb={2}/>
-            <Footer/>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <ButtonAppBar />
+      <Router />
+      <Box mt={5} mb={2} />
+      <Footer />
+    </ThemeProvider>
+  );
 }
 
 export default App;
